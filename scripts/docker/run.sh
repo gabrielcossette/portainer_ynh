628c762ff4f97e618d0ef9ff3f049d7668f53df7
Gabriel Cossette <gabriel@gcossette.com>
2020-03-14 19:10:16 -0400

    Update run.sh

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -3,7 +3,7 @@
 [ "$architecture" == "amd64" ] && image=linuxserver/bookstack
 [ -z $image ] && ynh_die "Sorry, your $architecture architecture is not supported ..."
 
-options="-p $port:80 -v $data_path/data:/config -e PUID=0 -e PGID=0 -e DB_HOST=localhost -e DB_USER=__DB_USER__ -e DB_PASS=__DB_PWD__ -e DB_DATABASE=__DB_USER__ -e APP_URL=https://__URL__"
+options="-p $port:80 -v $data_path/data:/config -e PUID=1000 -e PGID=1000 -e DB_HOST=localhost -e DB_USER=__DB_USER__ -e DB_PASS=__DB_PWD__ -e DB_DATABASE=__DB_USER__ -e APP_URL=https://__URL__"
 
 iptables -t filter -N DOCKER 
 
