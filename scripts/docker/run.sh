076c68a37bdd25c7f26a19ecfc226530a7e01954
Gabriel Cossette <gabriel@gcossette.com>
2020-03-14 18:31:24 -0400

    Update run.sh

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-[ "$architecture" == "amd64" ] && image=portainer/portainer:1.23.1
-[ "$architecture" == "i386" ]  && image=portainer/portainer:linux-386-1.23.1
-[ "$architecture" == "armhf" ] && image=portainer/portainer:linux-arm-1.23.1
+[ "$architecture" == "amd64" ] && image=linuxserver/bookstack
 [ -z $image ] && ynh_die "Sorry, your $architecture architecture is not supported ..."
 
-options="-p $port:9000 -v $data_path/data:/data -v /var/run/docker.sock:/var/run/docker.sock"
-containeroptions="--no-auth"
+options="-p $port:80 -v $data_path/data:/config -e PUID=0 -e PGID=0 -e DB_HOST=localhost -e DB_USER=__DB_USER__ -e DB_PASS=__DB_PWD__ -e DB_DATABASE=__DB_USER__"
 
 iptables -t filter -N DOCKER 
 
-docker run -d --name=$app --restart always $options $image $containeroptions 1>&2
+docker run -d --name=$app --restart always $options $image 1>&2
 CR=$?
 
 echo $CR
